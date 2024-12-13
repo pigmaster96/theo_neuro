ff569a74f8c1b557d1a28c0f782ec30525f5ec77
pigmaster96 <malcolmtan2095@gmail.com>
2024-12-13 17:25:43 +0000

    update

---
+++ miller/c1/LIF.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#LIF with fwd euler: 
+#dvdt=((E_l-V_m)+R*I_app)/Tau or (G_l*(E_l-V_m)+I_app))/C_m (same)
+
+
+
+
+
+
+
+
+
--- miller/c1/fwd_euler.py
+++ miller/c1/fwd_euler.py
@@ -2,10 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Forward Euler: dxdt=0.3x w/ initial condition x(0)=exp(0.3)
-dt=0.5
-
 #time vector
-t=np.arange(1,5+dt/2,dt)
+t=np.linspace(0,5,10)
 
 #get number of steps
 n=len(t)
@@ -22,6 +20,7 @@ for i in range(n-1): #-1 because we already set initial condition
 
 #plotting
 fig=plt.figure(figsize=(6,4))
-plt.plot(t,x)
+plt.plot(t,x,label='Euler Estimate')
+plt.plot(t,np.exp(0.3*t),label="Exact Solution")
+plt.legend()
 plt.show()
-
