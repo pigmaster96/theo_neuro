9c25f200553120a19afdc4088121603b9c176200
Malcolm <malcolmtan2095@gmail.com>
2025-10-26 02:10:02 +0800

    BACK

--- miller/c1/AELIF.py
+++ miller/c1/AELIF.py
@@ -2,16 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import trange
 import random
-from LIFclass import LIFneuron
 
 
-class AELIF(LIFneuron):
-    '''ELIF/AELIF model'''
-    
-    def __init__(self,E_l=-70,C_m=100,G_l=10,
-                 V_th=-50,V_r=-80,I_app=201,sigma_i=0.1):
-        super().__init__(E_l,C_m,G_l,
-                 V_th,V_r,I_app,sigma_i)
-        
+#AELIF Model
+
+def AELIFneuron(E_l=-75,V_th=-50,V_r=-80,delta_th=2,G_l=10,C_m=100,a=2,b=0.02,tau_sra=200,dt=0.001,tmax=1.5):
+    timevec=np.linspace(0,tmax,int(tmax/dt))
+
+       
+
+
+
+
+
+
+print(AELIFneuron())
+
 
 
